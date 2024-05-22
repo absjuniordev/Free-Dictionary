77affd3c7e4ece69db07f5e751d2d0920737b79f
Arnaldo Junior <abs.junnior@hotmail.com>
2024-05-21 23:32:52 -0300

    Criação do campo para inserção da palavra

--- lib/app/views/home_screen.dart
+++ lib/app/views/home_screen.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:free_dicionary/app/models/dictionary_model.dart';
+import 'package:free_dicionary/app/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final _appService = ApiService();
+    final _wordControlle = TextEditingController(text: "");
     return Scaffold(
       appBar: AppBar(
         title: const Text('Free Dictionary'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _wordControlle,
+              decoration: const InputDecoration(
+                  labelText: "Enter a word", border: OutlineInputBorder()),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Search"),
+          ),
+          FutureBuilder<DictionaryModel>(
+            future: _appService.fetchWord("word"),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else {
+                final word = snapshot.data!;
+                return Center(child: Text(word.word!));
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          color: Color.fromARGB(255, 185, 7, 7),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          ),
+        ),
+      ),
     );
   }
 }
