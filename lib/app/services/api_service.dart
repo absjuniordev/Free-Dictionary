ea06b13231d93c7c2aa93b5477e122f671f81602
Arnaldo Junior <abs.junnior@hotmail.com>
2024-05-21 22:38:20 -0300

    Criação do serviço de consulta a API

---
+++ lib/app/services/api_service.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:free_dicionary/app/models/dictionary_model.dart';
+
+class ApiService {
+  final dio = Dio();
+  static const _pathApi = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+  Future<DictionaryModel> fetchWord(String word) async {
+    try {
+      final response = await dio.get("$_pathApi$word");
+      return DictionaryModel.fromJson(response.data[0]);
+    } catch (e) {
+      debugPrint(e.toString());
+      throw Exception("Word not found");
+    }
+  }
+}
