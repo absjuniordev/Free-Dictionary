6095d014ab290e49926d5b8862c6070bd6af944f
Arnaldo Junior <abs.junnior@hotmail.com>
2024-05-21 23:38:52 -0300

    Implementação da função que obterar os dados e gerenciará

--- lib/app/provider/dictobary_provider.dart
+++ lib/app/provider/dictobary_provider.dart
@@ -1,1 +1,20 @@
-class DictobaryProvider {}
+import 'package:flutter/material.dart';
+import 'package:free_dicionary/app/models/dictionary_model.dart';
+import 'package:free_dicionary/app/services/api_service.dart';
+
+class DictobaryProvider extends ChangeNotifier {
+  final _apiService = ApiService();
+  DictionaryModel? _word;
+
+  DictionaryModel? get word => _word;
+
+  Future<void> fecthWord(String word) async {
+    try {
+      _word = await _apiService.fetchWord(word);
+      notifyListeners();
+    } catch (e) {
+      debugPrint(e.toString());
+      throw Exception("Failed to fetch word");
+    }
+  }
+}
