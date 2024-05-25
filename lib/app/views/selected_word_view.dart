f72764ae1143c6a795644d1431dd82e7fc34e748
Arnaldo Junior <abs.junnior@hotmail.com>
2024-05-25 19:27:28 -0300

    Acrescentando palavras reais ao app

--- lib/app/views/selected_word_view.dart
+++ lib/app/views/selected_word_view.dart
@@ -20,11 +20,20 @@ class SelectedWordView extends StatelessWidget {
           return const LoadingPageView();
         } else {
           final word = snapshot.data!;
-          final phoneticText = word.phonetics!
-              .firstWhere((phonetic) =>
-                  phonetic.text != null && phonetic.text!.isNotEmpty)
-              .text
-              ?.replaceAll('/', '');
+
+          final phoneticText =
+              word.phonetics != null && word.phonetics!.isNotEmpty
+                  ? word.phonetics!
+                      .firstWhere(
+                        (phonetic) =>
+                            phonetic.text != null && phonetic.text!.isNotEmpty,
+                        orElse: () => Phonetics(text: ""),
+                      )
+                      .text
+                      ?.replaceAll('/', '')
+                  : "";
+          final audioUrl = word.phonetics![0].audio;
+
           return Scaffold(
             body: Container(
               color: const Color.fromRGBO(106, 218, 238, 100),
@@ -41,32 +50,84 @@ class SelectedWordView extends StatelessWidget {
                   const SizedBox(height: 20),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: double.infinity,
-                      height: 300,
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        color: const Color.fromARGB(255, 175, 173, 100),
-                      ),
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              word.word!,
-                              style: const TextStyle(fontSize: 30),
-                            ),
-                            Text(phoneticText ?? '',
-                                style: const TextStyle(fontSize: 30)),
-                          ],
+                    child: Card(
+                      elevation: 5,
+                      child: Container(
+                        width: double.infinity,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          color: const Color.fromRGBO(255, 175, 173, 100),
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                word.word!,
+                                style: const TextStyle(fontSize: 30),
+                              ),
+                              Text(
+                                phoneticText ?? "",
+                                style: const TextStyle(fontSize: 30),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
                   const SizedBox(height: 15),
-                  Text("Audio"),
+                  const Text("Audio"),
                   const SizedBox(height: 15),
-                  Text("Meanings")
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Meanings",
+                      style:
+                          TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      '${word.meanings![0].partOfSpeech?[0].toUpperCase()}${word.meanings![0].partOfSpeech!.substring(1)}: ${word.meanings![0].definitions![0].definition}',
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(audioUrl!),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Card(
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Voltar",
+                                style: TextStyle(
+                                    fontSize: 25, color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Card(
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Proximo",
+                                style: TextStyle(
+                                    fontSize: 25, color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
