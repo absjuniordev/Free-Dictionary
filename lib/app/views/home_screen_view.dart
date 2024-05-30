267fd54dff3443e6679caf2b6d0ab51c1f45d4e1
Arnaldo Junior <abs.junnior@hotmail.com>
2024-05-29 22:33:53 -0300

    Substituição dos botões, pelo BottomNavigationBar

--- lib/app/views/home_screen_view.dart
+++ lib/app/views/home_screen_view.dart
@@ -2,39 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:free_dicionary/app/widgets/select_button_widget.dart';
 import 'package:free_dicionary/app/widgets/words_view_widget.dart';
 
+import '../provider/dictionary_provider.dart';
+
 class HomeScreenView extends StatelessWidget {
   const HomeScreenView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final injectorStore = getIt<DictionaryProvider>();
+
     return Scaffold(
-      body: Stack(
+      bottomNavigationBar: ListenableBuilder(
+        listenable: injectorStore,
+        builder: (_, __) => BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'Words List',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+          ],
+          currentIndex: injectorStore.activeIndex,
+          selectedItemColor: const Color.fromARGB(255, 0, 0, 0),
+          onTap: injectorStore.selectedButton,
+        ),
+      ),
+      body: Column(
         children: [
-          Column(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Container(
-                  color: const Color.fromRGBO(42, 117, 207, 100),
-                ),
-              ),
-              Expanded(
-                flex: 9,
-                child: Container(
-                  color: const Color.fromRGBO(106, 218, 238, 100),
-                  child: const WordsViewWidget(),
-                ),
-              ),
-            ],
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: const Color.fromRGBO(42, 117, 207, 100),
+            ),
           ),
-          const Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            top: -545,
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: SelectButtonWidget(),
+          Expanded(
+            flex: 9,
+            child: Container(
+              color: const Color.fromRGBO(106, 218, 238, 100),
+              child: const WordsViewWidget(),
             ),
           ),
         ],
